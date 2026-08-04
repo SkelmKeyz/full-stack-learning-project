f10f49ca9c0388bf4e18ca376f726891939fa3bd
SkelmKeyz <k.jleseba@gmail.com>
2026-08-04 15:06:00 +0200

    first commit to git

--- app.js
+++ app.js
@@ -9,13 +9,84 @@ app.use(cors({
     origin: ['http://localhost:5500', 'http://127.0.0.1:5500']
 }));
 
+// JSON parser first
+app.use(express.json());
+
 // using the public folder at the root of the project
 app.use(express.static("public"));
 
 // using the router
 app.use('/images', express.static('images'));
 
-app.use(express.json());
+// using express.json and urlencoded
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }));
+
+
+// Get with next
+app.get('/next', (req, res, next) => {
+    console.log('The ignition key');
+    next();
+}, (req, res) => {
+    res.send('Ignition route sep up with a second callback')
+})
+
+// Get - redirect method
+app.get('/redirect', (req, res) => {
+    res.redirect('http://www.google.com');
+})
+
+//route chaining
+//Get
+
+app.route('/class', (req, res) => {
+    res.send('http://www.google.com');
+})
+
+   .put((req, res) => {
+    res.send('This is a PUT resquest at /edit');
+})
+
+   .delete((req, res) => {
+    res.send('This is a DELETE request at /delete')
+})
+   .get((req, res) => {
+    res.send(data);
+})
+
+//Get
+app.get('/', (req, res) => {
+    res.json(data);
+})
+
+//Post - express.json and express.urlencoded
+app.post('/item', (req, res) => {
+  const { name, item } = req.body;
+  console.log('New item:', name, item);
+
+  res.json({
+    message: 'Item received successfully',
+    name,
+    item
+  });
+});
+
+
+
+// Get with routing parameters
+app.get('/class/:id', (req, res) => {
+    // Middleware: Acess the routing parameters
+    const StudentId = Number(req.params.id); 
+    
+    const Student = data.filter(item => item.id === StudentId);
+//Everything above this line is Middleware
+    res.json(Student);
+})
+
+
+
+
 
 app.use('/products', productsRouter);
 
@@ -43,18 +114,12 @@ app.post('/message', (req, res) => {
     res.json({ message: 'Thank you for your cooporation'})
 })
 
-app.put('/put', (req, res) => {
-    res.send('This is a PUT resquest at /edit');
-})
 
-app.delete('/delete', (req, res) => {
-    res.send('This is a DELETE request at /delete')
-})
 
 
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`); 
-    console.log(data);
+    console.log(`Loaded ${data.length} records`);
 });
 
